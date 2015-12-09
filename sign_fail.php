19315642b8ecd42fe56710f1917c05b551b4ada6
shengshenggu <s102213020@ncnu.edu.tw>
2015-12-09 23:59:00 +0800

    登入失敗頁面

---
+++ sign_fail.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Happy Farm</title>
+<style type="text/css">
+
+#frame {
+position: absolute;
+top: 20%; 
+left: 25%; 
+width: 960px; 
+}
+#frame img { 
+max-width:100%; 
+width:expression(document.body.clientWidth>document.getElementById("frame").scrollWidth*8/10? "100%": "auto" );
+heitht:auto;
+}
+
+}
+</style>
+</head>
+<body>
+<div id="frame">
+<img id="fail" src="img\fail.jpg" />
+</div>
+<?php
+    header("Refresh: 3; url=signup.html");
+?>
+</body>
+</html>
