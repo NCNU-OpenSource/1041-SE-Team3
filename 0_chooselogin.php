95830d6297a197f37cb3831e878a2d5d641bd9cc
shengshenggu <s102213020@ncnu.edu.tw>
2015-12-09 21:13:20 +0800

    選擇登入或註冊

---
+++ 0_chooselogin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Happy Farm</title>
+<style type="text/css">
+
+#frame {
+position: absolute;
+top: 20%; 
+left: 25%; 
+width: 960px; 
+}
+#frame img { 
+max-width:100%; 
+width:expression(document.body.clientWidth>document.getElementById("frame").scrollWidth*8/10? "100%": "auto" );
+heitht:auto;
+}
+#clogin{
+position: absolute;
+top:180px;
+left: 300px;
+}
+#csign{
+position: absolute;
+top:320px;
+left: 300px;
+}
+</style>
+
+</head>
+<body>
+<div id="frame">
+<img id="farm1" src="img\farm1.gif" />
+<a href="login.html"><img id="clogin" src="img\clogin.png"></a>
+<a href="0_signup.php"><img id="csign" src="img\csign.png"></a>
+</div>
+</body>
+</html>
