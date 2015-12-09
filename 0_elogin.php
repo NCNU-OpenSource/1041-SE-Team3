f8e01d2c06dc51886012f545d9f24b91cd5a80e2
shengshenggu <s102213020@ncnu.edu.tw>
2015-12-09 23:52:29 +0800

    登入錯誤的頁面

---
+++ 0_elogin.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Refresh: 3; url=0_chooselogin.php");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Happy Farm</title>
+<style type="text/css">
+
+#frame {
+position: absolute;
+top: 20%; 
+left: 25%; 
+width: 960px; 
+}
+
+</style>
+</head>
+<body>
+<div id="frame">
+<img id="errorlogin" src="img\errorlogin.jpg" />
+</div>
+</body>
+</html>
