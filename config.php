46000441657165501578b000b8a9bb283263718d
shengshenggu <s102213020@ncnu.edu.tw>
2015-12-19 21:39:58 +0800

    連接資料庫

--- config.php
+++ config.php
@@ -8,4 +8,11 @@ $db = 'farmdb';
 $conn = mysqli_connect($host, $user, $pass,$db) or die('Error with MySQL connection'); 
 mysqli_query($conn,"SET NAMES utf8"); 
 
+if(!isset($_SESSION["uid"])) {
+		header("Location: 0_login.php");
+    }
+if($_SESSION["uid"] == "" ){ 
+		header("Location: 0_login.php");
+    }
+
 ?>
