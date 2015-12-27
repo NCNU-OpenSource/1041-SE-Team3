773c255e18bf03dc9b233d1244a8602719074801
102213044 <s102213044@mail1.ncnu.edu.tw>
2015-12-27 14:14:11 +0800

    種植

---
+++ grow.php
@@ -0,0 +1,35 @@
+<?php
+include"config.php";
+$id=$_SESSION['uid'];
+$lid=$_SESSION['glid'];
+$sid=$_GET['gsid'];
+
+$sql = "select * from seed where `sid`='$sid';";
+$results = mysqli_query($conn,$sql);
+while($rs=mysqli_fetch_array($results)){
+	$sexp = $rs['sexp'];
+	$senergy = $rs['senergy'];
+	$stime = $rs['stime'];
+}
+$sql4 = "select * from user where `uid`='$id';";
+$result1 = mysqli_query($conn,$sql4);
+while($rs1=mysqli_fetch_array($result1)){
+	$uenergy = $rs1['uenergy'];
+}
+
+if($uenergy < $senergy)
+	echo "體力不足，無法種植。<a href='1_main.php'>返回</a>";
+else{
+	$sql1 = "update land set `lstatus`=2, `ltime`='$stime' where `lid`='$lid' and `uid`='$id';";
+	mysqli_query($conn,$sql1);
+	
+	$sql2 = "update warehouse set scount=scount-1 where `uid`='$id' and `sid`='$sid';";
+	mysqli_query($conn,$sql2);
+	
+	$sql3 = "update user set uexp=uexp+$sexp, uenergy=uenergy-$senergy where `uid`='$id';";
+	mysqli_query($conn,$sql3);
+	
+	header("Location:1_main.php");
+}
+
+?>
