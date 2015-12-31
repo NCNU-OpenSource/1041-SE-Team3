97f4bf0b277dec16233adea6ef187c3213bc1e10
s102213052 <s102213052@mail1.ncnu.edu.tw>
2015-12-31 16:56:48 +0800

    倉庫_種子存量
    重新排版

--- 2_warehouse_seed.php
+++ 2_warehouse_seed.php
@@ -28,30 +28,8 @@ $id=$_SESSION['uid'];
     left: 90px;
     font-weight: bold;
     font-family:  Arimo;
-    font-size:40px;
+    font-size:30px;
     color:white;
-}
-#list2 { 
-    position: absolute;
-    border:0; 
-    border-collpase:collpase;  
-    top: 150px;
-    left: 355px;
-    font-weight: bold;
-    font-family:  Arimo;
-    font-size:40px;
-    color:white;
-}
-#list3 { 
-    position: absolute;
-    border:0; 
-    border-collpase:collpase;  
-    top: 150px;
-    left: 620px;
-    font-weight: bold;
-    font-family:  Arimo;
-    font-size:40px;
-	color:white;
 }
 #menu{
     position: absolute;
@@ -80,79 +58,63 @@ $id=$_SESSION['uid'];
 td {
     text-align: center;
 }
-#land3{
+#cancel{
     position: absolute;
-    top: 240px;
-    left: 340px;
+    top: 30px;
+    left: 860px;
 }
-#land4{
-    position: absolute;
-    top: 360px;
-    left: 340px;
-}
-#land5{
-    position: absolute;
-    top: 240px;
-    left: 460px;
-}
-#land6{
-    position: absolute;
-    top: 360px;
-    left: 460px;
-}
-#land7{
-    position: absolute;
-    top: 240px;
-    left: 580px;
-}
-}
+
 </style>
 </head>
 <body>
 <div id="frame">
 <a href="1_main.php"><img src="img\dark.png" /></a>
 <img id="menu" src="img\warehouse_seed.png" />
+<a href="1_main.php"><img id="cancel" src="img\cancel.png" /></a>
 <a href="2_warehouse.php"><p id="product">成品</p></a>
 <a href="2_warehouse_seed.php"><p id="seed">種子</p></a>
 <table id='list1' >
-<?php
-	$sql = "select * from warehouse where uid='$id'";
+<tr><td><img src='img\tomato.png' width='115' height='110'></td>
+<?php //獲得tomato的價格
+	$sql = "select * from warehouse where pid = 'tomato' ;";
     $results=mysqli_query($conn,$sql);	
-	
-	while ($rs=mysqli_fetch_array($results)) {    
-        if($rs['sid']=='tomato'){
-            echo "<tr><td><img src=\"img\stomato.png\" width='130' height='130'></td><td width ='100px'>X",$rs['scount'],"</td></tr>";
-        }
-        if($rs['sid']=='eggplant'){
-            echo "<tr><td><img src=\"img\seggplant.png\" width='130' height='130'></td><td>X",$rs['scount'],"</td></tr>";
-        }
-    }
+	while (	$rs=mysqli_fetch_array($results)) {
+		echo "<td width = 170px>",$rs['scount'] ," unit</td>" ;
+	}
 ?>
-</table>
-<table id='list2' >
-<?php
-	$sql = "select * from warehouse where uid='$id'";
+<td><img src='img\eggplant.png' width='95' height='95'></td>
+<?php //獲得tomato的價格
+	$sql = "select * from warehouse where pid = 'eggplant' ;";
     $results=mysqli_query($conn,$sql);	
-	while ($rs=mysqli_fetch_array($results)) {    
-        if($rs['sid']=='beetroot'){
-            echo "<tr><td><img src=\"img\sbeetroot.png\" width='130' height='130'></td><td width ='100px'>X",$rs['scount'],"</td></tr>";
-        }
-        if($rs['sid']=='carrot'){
-            echo "<tr><td><img src=\"img\scarrot.png\" width='130' height='130'></td><td>X",$rs['scount'],"</td></tr>";
-        }
-    }
+	while (	$rs=mysqli_fetch_array($results)) {
+		echo "<td width = 170px>",$rs['scount'] ," unit</td>" ;
+	}
 ?>
-</table>
-<table id='list3' >
-<?php
-	$sql = "select * from warehouse where uid='$id'";
+<td><img src='img\carrot.png' width='95' height='95'></td>
+<?php //獲得tomato的價格
+	$sql = "select * from warehouse where pid = 'carrot' ;";
     $results=mysqli_query($conn,$sql);	
-	while ($rs=mysqli_fetch_array($results)) {    
-        if($rs['sid']=='yellowbean'){
-            echo "<tr><td><img src=\"img\syellowbean.png\" width='130' height='130'></td><td width ='100px'>X",$rs['scount'],"</td></tr>";
-        }
-    }
+	while (	$rs=mysqli_fetch_array($results)) {
+		echo "<td width = 170px>",$rs['scount'] ," unit</td>" ;
+	}
 ?>
+<tr><td><img src='img\beetroot.png' width='95' height='95'></td>
+<?php //獲得tomato的價格
+	$sql = "select * from warehouse where pid = 'beetroot' ;";
+    $results=mysqli_query($conn,$sql);	
+	while (	$rs=mysqli_fetch_array($results)) {
+		echo "<td width = 170px>",$rs['scount'] ," unit</td>" ;
+	}
+?>
+<td><img src='img\yellowbean.png' width='95' height='95'></td>
+<?php //獲得tomato的價格
+	$sql = "select * from warehouse where pid = 'yellowbean' ;";
+    $results=mysqli_query($conn,$sql);	
+	while (	$rs=mysqli_fetch_array($results)) {
+		echo "<td width = 170px>",$rs['scount'] ," unit</td>" ;
+	}
+?>
+
 </table>
 </div>
 </body>
