71c1294142e730d363a97c28c5cf913270a0a177
102213044 <s102213044@mail1.ncnu.edu.tw>
2016-01-06 22:03:14 +0800

    修改bug

--- harvest.php
+++ harvest.php
@@ -6,35 +6,40 @@ $a=(int)$_GET['a'];
 $sql = "select * from user where `uid`='$id';";
 $result=mysqli_query($conn,$sql);
 $rs=mysqli_fetch_array($result);
+$OK = 0;
 
 if($a==1){//金錢
-	if($rs['umoney']<50)
+	if($rs['umoney']<50){
 		header("Location:money_alert.php");
+		$OK = 1;
+	}
 	else{
 		$sql1 = "update user set umoney=umoney-50 , uexp=uexp+15 where `uid`='$id';";
 		mysqli_query($conn,$sql1);
 	}
 }
 else if($a==2){//體力
-	if($rs['uenergy']<1)
+	if($rs['uenergy']<1){
 		header("Location:energy_alert.php");
+		$OK = 1;
+	}
 	else{
 		$sql1 = "update user set uenergy=uenergy-1, uexp=uexp+15 where `uid`='$id';";
 		mysqli_query($conn,$sql1);
 	}
 }
-$sql4 = "select * from land where `lid`='$lid' and `uid`='$id';";
-$results=mysqli_query($conn,$sql4);
-$rs1=mysqli_fetch_array($results);
-$sid=$rs1['sid'];
+if($OK == 0){
+	$sql4 = "select * from land where `lid`='$lid' and `uid`='$id';";
+	$results=mysqli_query($conn,$sql4);
+	$rs1=mysqli_fetch_array($results);
+	$sid=$rs1['sid'];
 
-$sql2 = "update land set `lstatus`=0,`ltime`=NULL,`sid`=NULL where `lid`='$lid' and `uid`='$id';";
-mysqli_query($conn,$sql2);
-	
-$sql3 = "update warehouse set pcount=pcount+1 where `uid`='$id' and `pid`='$sid';";
-mysqli_query($conn,$sql3);
+	$sql2 = "update land set `lstatus`=0,`ltime`=NULL,`sid`=NULL where `lid`='$lid' and `uid`='$id';";
+	mysqli_query($conn,$sql2);
+		
+	$sql3 = "update warehouse set pcount=pcount+1 where `uid`='$id' and `pid`='$sid';";
+	mysqli_query($conn,$sql3);
 
-header("Location:1_main.php");
-
-
+	header("Location:1_main.php");
+}
 ?>
