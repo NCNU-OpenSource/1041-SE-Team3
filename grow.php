4976ea09aeda4841994d69e11842f6c59a046795
s102213052 <s102213052@mail1.ncnu.edu.tw>
2016-01-06 14:40:54 +0800

    grow

--- grow.php
+++ grow.php
@@ -3,16 +3,18 @@ include"config.php";
 $id=$_SESSION['uid'];
 $lid=$_SESSION['glid'];
 $sid=$_SESSION['gsid'];
-$nowtime=time();
+//$nowtime=time();
+$time=date('U')+25200;
+
 $sql = "select * from seed where `sid`='$sid';";
 $results = mysqli_query($conn,$sql);
 while($rs=mysqli_fetch_array($results)){
 	$sexp = $rs['sexp'];
 	$senergy = $rs['senergy'];
-	$stime = $rs['stime'];
-	$parsed = date_parse($stime);
-	$ftime = $parsed['hour'] * 3600 + $parsed['minute'] * 60 + $parsed['second'] + $nowtime;
 }
+
+
+
 $sql4 = "select * from user where `uid`='$id';";
 $result1 = mysqli_query($conn,$sql4);
 while($rs1=mysqli_fetch_array($result1)){
@@ -21,16 +23,21 @@ while($rs1=mysqli_fetch_array($result1)){
 if($uenergy < $senergy)
 	header("Location:energy_alert.php");
 else{
-	$sql1 = "update land set `lstatus`=2,`ltime`='$ftime',`sid`='$sid' where `lid`='$lid' and `uid`='$id';";
-	mysqli_query($conn,$sql1);
+	$sql0 = "select * from seed where `sid`='$sid';";
+	$results = mysqli_query($conn,$sql0);
+    while($rs1=mysqli_fetch_array($results)){
+	    $sql1 = "update land set `lstatus`=2, ltime='".$time."'+'".$rs1['stime']."' ,`sid`='$sid' where `lid`='$lid' and `uid`='$id';";
+	    mysqli_query($conn,$sql1);
 	
-	$sql2 = "update warehouse set scount=scount-1 where `uid`='$id' and `sid`='$sid';";
-	mysqli_query($conn,$sql2);
+	    $sql2 = "update warehouse set scount=scount-1 where `uid`='$id' and `sid`='$sid';";
+	    mysqli_query($conn,$sql2);
 	
-	$sql3 = "update user set uexp=uexp+$sexp, uenergy=uenergy-$senergy where `uid`='$id';";
-	mysqli_query($conn,$sql3);
+	    $sql3 = "update user set uexp=uexp+".$sexp.", uenergy=uenergy-".$senergy." where `uid`='$id';";
+	    mysqli_query($conn,$sql3);
+
+	    header("Location:1_main.php");
+	}
 	
-	header("Location:1_main.php");
 }
 
 ?>
